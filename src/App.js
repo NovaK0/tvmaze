1c62ee19f7691791d7bdb1e93bb12a66ffc42e00
NovaK0 <rameezraja3460@gmail.com>
2022-12-02 02:25:19 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import {useEffect,useState} from "react"
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Routes, Route, HashRouter,BrowserRouter  } from "react-router-dom";
 import { Home } from './components/home/Home';
 import { Details } from './components/Details';
 function App() {
@@ -12,12 +12,12 @@ function App() {
        <div className='navbar'>
        <div className='big_text'>TV Maze</div>
         </div>
-     <BrowserRouter>
+    <HashRouter>
      <Routes>
-         <Route index element={<Home/>} /> 
-         <Route path="/Details/:id" element={<Details/>} /> 
+         <Route exact path="/" element={<Home/>} /> 
+         <Route exact path="/Details/:id" element={<Details/>} /> 
      </Routes>
-   </BrowserRouter>
+    </HashRouter>
 
 
     </div>
